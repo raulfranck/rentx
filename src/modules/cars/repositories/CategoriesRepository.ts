895effafbc9f2824e3f785cad4e60b185ea0dba4
raulfranck <raulengfranck@gmail.com>
2021-05-13 22:37:23 -0300

    Conhecendo Singleton Pattern

--- src/modules/cars/repositories/CategoriesRepository.ts
+++ src/modules/cars/repositories/CategoriesRepository.ts
@@ -4,11 +4,22 @@ import {
     ICreateCategoryDTO,
 } from "./ICategoriesRepository";
 
+// Padrão Singleton
+
 class CategoriesRepository implements ICategoriesRepository {
     private categories: Category[];
 
-    constructor() {
+    private static INSTANCE: CategoriesRepository;
+
+    private constructor() {
         this.categories = [];
+    }
+
+    public static getInstance(): CategoriesRepository {
+        if (!CategoriesRepository.INSTANCE) {
+            CategoriesRepository.INSTANCE = new CategoriesRepository();
+        }
+        return CategoriesRepository.INSTANCE;
     }
 
     create({ description, name }: ICreateCategoryDTO): void {
