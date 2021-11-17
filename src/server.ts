bc80e7d6189daec04830c71cbcf544f7ed06150b
raulfranck <raulengfranck@gmail.com>
2021-11-16 22:26:32 -0300

    nstalando TypeORM, postgres

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,14 @@
-import express from "express";
+import express, {RequestHandler} from "express";
 import swaggerUi from "swagger-ui-express";
 
 import swaggerFile from "./swagger.json";
+import "./database"
 
 import { router } from "./routes";
 
 const app = express();
 
-app.use(express.json());
+app.use(express.json() as RequestHandler);
 
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerFile))
 
