05b7eba2e24e1b6bd9e6c3bc83f7c90b15ecf630
raulfranck <raulengfranck@gmail.com>
2021-11-29 15:29:45 -0300

    Refatorando as especificações

--- src/modules/cars/useCases/importCategory/importCategoryController.ts
+++ src/modules/cars/useCases/importCategory/importCategoryController.ts
@@ -1,12 +1,14 @@
 import {Request, Response} from "express"
 import { ImportCategoryUseCase } from "./importCateogryUseCase";
+import {container} from "tsyringe";
 
 class ImportCategoryController {
-    constructor(private importCategoryUseCase: ImportCategoryUseCase) {}
-    handle(request: Request, response: Response): Response {
-        const { file } = request;
 
-        this.importCategoryUseCase.execute(file)
+    async handle(request: Request, response: Response): Promise<Response> {
+        const { file } = request;
+        const importCategoryUseCase = container.resolve(ImportCategoryUseCase)
+
+        await importCategoryUseCase.execute(file)
         return response.send()
     }
 }
--- src/routes/specifications.routes.ts
+++ src/routes/specifications.routes.ts
@@ -1,11 +1,10 @@
 import { Router } from "express";
 
-import { createSpecificationController } from "../modules/cars/useCases/createSpecifications";
+import { CreateSpecificationController } from "../modules/cars/useCases/createSpecifications/CreateSpecificationController";
 
 const specificationsRoutes = Router();
+const createSpecificationController = new CreateSpecificationController()
 
-specificationsRoutes.post("/", (request, response) => {
-    return createSpecificationController.handle(request, response);
-});
+specificationsRoutes.post("/", createSpecificationController.handle);
 
 export { specificationsRoutes };
