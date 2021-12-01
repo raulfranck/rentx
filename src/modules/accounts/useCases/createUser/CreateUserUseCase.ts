d3d63a519cd5d6f6d194a90d70692f8c24c4e2b9
raulfranck <raulengfranck@gmail.com>
2021-12-01 18:00:28 -0300

    Criptografar senha + Error usuario ja existe

--- src/modules/accounts/useCases/createUser/CreateUserUseCase.ts
+++ src/modules/accounts/useCases/createUser/CreateUserUseCase.ts
@@ -1,4 +1,5 @@
 import { inject, injectable } from "tsyringe";
+import { hash } from "bcryptjs";
 
 import { IUsersRepository } from "../../repositories/IUserRepository";
 import { ICreateUserDTO } from "../../dtos/ICreateUserDTO";
@@ -17,10 +18,18 @@ class CreateUserUseCase {
         password, 
         driver_license
     }: ICreateUserDTO): Promise<void> {
+        const userAlreadyExists = await this.usersRepository.findByEmail(email)
+
+        if(userAlreadyExists) {
+            throw new Error("User already exists");
+        }
+
+        const passwordHash = await hash(password, 8);
+
         await this.usersRepository.create({
             name, 
             email, 
-            password, 
+            password: passwordHash, 
             driver_license,
         })
     }
